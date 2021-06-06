84c14b3b9c80e2b6dd1f1ffd0069c6f31a1a9304
Dylan Conway <dylan.conway567@gmail.com>
2021-06-05 19:11:01 -0700

    added color vectors

--- src/defines.h
+++ src/defines.h
@@ -4,4 +4,11 @@
 #define WINDOW_W (1920 / 4 * 3)
 #define WINDOW_H (1080 / 4 * 3)
 
+#define WHITE               (vec4){1.0f,1.0f,1.0f,1.0f}
+#define BLACK               (vec4){0.0f,0.0f,0.0f,1.0f}
+#define GRAY                (vec4){0.5f,0.5f,0.5f,1.0f}
+#define RED                 (vec4){1.0f,0.0f,0.0f,1.0f}
+#define ORANGE              (vec4){255.0f/255.0f,165.0f/255.0f,0.0f,1.0f}
+#define CORNFLOWERBLUE      (vec4){100.0f/255.0f,149.0f/255.0f,237.0f/255.0f,1.0f}
+
 #endif
